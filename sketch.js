de6635e68ef01fbad9b3b2d8cc44f2448c1bc80f
mrepol742 <mrepol742@gmail.com>
2022-11-05 09:28:13 +0800

    Initial Commit

--- sketch.js
+++ sketch.js
@@ -33,3 +33,17 @@ canvas.addEventListener("mousemove", (e) => {
   ctx.lineTo(e.clientX, e.clientY);
   ctx.stroke();
 });
+
+canvas.addEventListener("touchmove", (e) => {
+  //if (!painting) return;
+
+  ctx.lineWidth = 4.5;
+  ctx.lineCap = "round";
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    ctx.strokeStyle = "#ffffff";
+} else {
+  ctx.strokeStyle = "#212121";
+}
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
+});
